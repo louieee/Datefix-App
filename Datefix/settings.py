ea346862ecaa4b705b1e6d76523a8e3f14d7e608
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-10-26 20:30:29 +0100

    add aws rds

--- Datefix/settings.py
+++ Datefix/settings.py
@@ -91,12 +91,19 @@ ASGI_APPLICATION = 'Datefix.routing.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
+        "PORT": config('DB_PORT'),
+        "OPTIONS": {
+            "sslmode": "verify-ca",
+            "sslrootcert": os.path.join(BASE_DIR, "rds-combined-ca-bundle.pem")
+        }
     }
 }
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
