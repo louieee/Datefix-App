ef72efa9500614cf002630e4de9b9829025ca3d8
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-10-28 23:07:19 +0100

    Chore: update settings.py

--- Datefix/settings.py
+++ Datefix/settings.py
@@ -112,13 +112,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 
-EMAIL_USE_SSL = False
+EMAIL_USE_SSL = True
 
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 
-EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_USER = 'Louisane'
 
 EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY')
 
