231967d2ed09607127acaa6aab48d01c07c50268
Louis Ohaegbu <louis.paul9095@gmail.com>
2020-11-02 03:22:43 +0100

    fix url bug

--- Payment/urls.py
+++ Payment/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('redirect/', redirect_match, name='redirect_match'),
-    path('test/', tester, name='testrer'),
     path('rave/redirect/<int:user_id>/<package>/<duration>/<tx_ref>/', rave_redirect, name='rave_redirect'),
     path('rave/webhook/', rave_webhook, name='rave_webhook'),
     path('pay/', rave_pay, name='pay'),
