b8797d6cd2ebe507126707ac344d317e022d0622
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2023-11-04 16:56:28 +0100

    merge latest updates

--- Account/models.py
+++ Account/models.py
@@ -7,94 +7,94 @@ from django.db.models import Q
 
 
 class User(AbstractUser):
-	sex = models.CharField(max_length=6, default=None, null=True)
-	phone = models.CharField(max_length=16, default=None, null=True)
-	user_data = models.JSONField(default=dict)
-	choice_data = models.JSONField(default=dict)
-	deal_breaker = models.JSONField(default=list)
-	profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
-	matches = models.JSONField(default=list)
-	successful_matches = models.JSONField(default=list)
-	no_matches = models.JSONField(default=list)
-	jilted_matches = models.JSONField(default=list)
-	couple_ids = models.JSONField(default=list)
-	session = models.IntegerField(default=-1)
-	verified = models.BooleanField(default=False)
-	status = models.CharField(max_length=64, default='Offline')
-	can_be_matched = models.BooleanField(default=False)
-	extra_support = models.BooleanField(default=False)
+    sex = models.CharField(max_length=6, default=None, null=True)
+    phone = models.CharField(max_length=16, default=None, null=True)
+    user_data = models.JSONField(default=dict)
+    choice_data = models.JSONField(default=dict)
+    deal_breaker = models.JSONField(default=list)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
+    matches = models.JSONField(default=list)
+    successful_matches = models.JSONField(default=list)
+    no_matches = models.JSONField(default=list)
+    jilted_matches = models.JSONField(default=list)
+    couple_ids = models.JSONField(default=list)
+    session = models.IntegerField(default=-1)
+    verified = models.BooleanField(default=False)
+    status = models.CharField(max_length=64, default='Offline')
+    can_be_matched = models.BooleanField(default=False)
+    extra_support = models.BooleanField(default=False)
     email = models.EmailField(unique=True, validators=[validate_email])
 
-	@property
-	def complete_match(self):
-		return self.matches.__len__() >= 2
+    @property
+    def complete_match(self):
+        return self.matches.__len__() >= 2
 
-	@property
-	def is_couple(self):
-		return Couple.objects.filter(Q(first_partner=self) | Q(second_partner=self)).exists()
+    @property
+    def is_couple(self):
+        return Couple.objects.filter(Q(first_partner=self) | Q(second_partner=self)).exists()
 
-	@property
-	def origin(self):
-		return self.user_data.get('origin_state', '')
+    @property
+    def origin(self):
+        return self.user_data.get('origin_state', '')
 
-	@property
-	def residence(self):
-		return f"{self.user_data.get('residence_address', '')} {self.user_data.get('residence_state', '')}"
+    @property
+    def residence(self):
+        return f"{self.user_data.get('residence_address', '')} {self.user_data.get('residence_state', '')}"
 
-	@property
-	def religion(self):
-		return self.user_data.get('religion', '')
+    @property
+    def religion(self):
+        return self.user_data.get('religion', '')
 
-	@property
-	def denomination(self):
-		return self.user_data.get('denomination', '')
+    @property
+    def denomination(self):
+        return self.user_data.get('denomination', '')
 
-	@property
-	def has_children(self):
-		return self.user_data.get('children', '')
+    @property
+    def has_children(self):
+        return self.user_data.get('children', '')
 
-	def personality(self):
-		test = PersonalityTest.objects.filter(email=self.email).first()
-		return test.titles() if test else list()
+    def personality(self):
+        test = PersonalityTest.objects.filter(email=self.email).first()
+        return test.titles() if test else list()
 
-	@property
-	def chats(self):
-		from Chat.models import ChatThread
-		return ChatThread.objects.filter(Q(first_user_id=self.id) | Q(second_user_id=self.id)).values_list("id",
-		                                                                                                   flat=True)
+    @property
+    def chats(self):
+        from Chat.models import ChatThread
+        return ChatThread.objects.filter(Q(first_user_id=self.id) | Q(second_user_id=self.id)).values_list("id",
+                                                                                                           flat=True)
 
 
 class Couple(models.Model):
-	first_partner = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='match1')
-	second_partner = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='match2')
-	couple_name = models.CharField(max_length=30, default='')
-	datetime = models.DateTimeField()
+    first_partner = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='match1')
+    second_partner = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='match2')
+    couple_name = models.CharField(max_length=30, default='')
+    datetime = models.DateTimeField()
 
-	def true_details(self, user_id):
-		if self.first_partner_id == user_id:
-			user = User.objects.filter(id=self.second_partner_id).first()
-		elif self.second_partner_id == user_id:
-			user = User.objects.filter(id=self.first_partner_id).first()
-		else:
-			return None
-		if user is None:
-			return None
-		return {"firstName": user.first_name,
-		        "lastName": user.last_name, "phone": user.phone, "email": user.email,
-		        "residential_address": user.residence,
-		        "origin_address": f"{user.user_data.get('origin_lga', '')}, {user.origin}"}
+    def true_details(self, user_id):
+        if self.first_partner_id == user_id:
+            user = User.objects.filter(id=self.second_partner_id).first()
+        elif self.second_partner_id == user_id:
+            user = User.objects.filter(id=self.first_partner_id).first()
+        else:
+            return None
+        if user is None:
+            return None
+        return {"firstName": user.first_name,
+                "lastName": user.last_name, "phone": user.phone, "email": user.email,
+                "residential_address": user.residence,
+                "origin_address": f"{user.user_data.get('origin_lga', '')}, {user.origin}"}
 
 
 class PersonalityTest(models.Model):
-	email = models.EmailField()
-	extraversion = models.JSONField(default=dict)
-	neurotism = models.JSONField(default=dict)
-	agreeableness = models.JSONField(default=dict)
-	conscientiousness = models.JSONField(default=dict)
-	openness = models.JSONField(default=dict)
+    email = models.EmailField()
+    extraversion = models.JSONField(default=dict)
+    neurotism = models.JSONField(default=dict)
+    agreeableness = models.JSONField(default=dict)
+    conscientiousness = models.JSONField(default=dict)
+    openness = models.JSONField(default=dict)
 
-	def titles(self):
-		real = lambda x: json.loads(x)['title'] if 'title' in json.loads(x) else ''
-		return tuple([real(self.extraversion), real(self.neurotism),
-		              real(self.agreeableness), real(self.conscientiousness),
-		              real(self.openness)])
+    def titles(self):
+        real = lambda x: json.loads(x)['title'] if 'title' in json.loads(x) else ''
+        return tuple([real(self.extraversion), real(self.neurotism),
+                      real(self.agreeableness), real(self.conscientiousness),
+                      real(self.openness)])
--- Chat/models.py
+++ Chat/models.py
@@ -10,135 +10,135 @@ from Account.models import User
 
 
 class ChatThread(models.Model):
-	first_user = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='chatter1')
-	second_user = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='chatter2')
-	show_first = models.BooleanField(default=True)
-	show_second = models.BooleanField(default=True)
-	date_created = models.DateTimeField()
-	date_first = models.BooleanField(default=None, blank=True, null=True)
-	date_second = models.BooleanField(default=None, blank=True, null=True)
-	show_details = models.BooleanField(default=False)
-	first_deleted = models.JSONField(default=list)
-	second_deleted = models.JSONField(default=list)
-	secret = models.BinaryField()
-	expiry_date = models.DateTimeField(default=None)
-	last_message_date = models.DateTimeField(default=None, null=True)
+    first_user = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='chatter1')
+    second_user = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING, related_name='chatter2')
+    show_first = models.BooleanField(default=True)
+    show_second = models.BooleanField(default=True)
+    date_created = models.DateTimeField()
+    date_first = models.BooleanField(default=None, blank=True, null=True)
+    date_second = models.BooleanField(default=None, blank=True, null=True)
+    show_details = models.BooleanField(default=False)
+    first_deleted = models.JSONField(default=list)
+    second_deleted = models.JSONField(default=list)
+    secret = models.BinaryField()
+    expiry_date = models.DateTimeField(default=None)
+    last_message_date = models.DateTimeField(default=None, null=True)
 
-	def chat_name(self):
-		return f'User_{self.first_user_id} and User_{self.second_user_id}'
+    def chat_name(self):
+        return f'User_{self.first_user_id} and User_{self.second_user_id}'
 
-	def self_delete(self):
-		if not self.show_first and not self.show_second:
-			self.delete()
+    def self_delete(self):
+        if not self.show_first and not self.show_second:
+            self.delete()
 
-		if not self.date_first or not self.date_second:
-			self.delete()
+        if not self.date_first or not self.date_second:
+            self.delete()
 
-	def show_detail(self):
-		if (timezone.now() - self.date_created).days == 7:
-			self.show_details = True
-			self.save()
-		return
+    def show_detail(self):
+        if (timezone.now() - self.date_created).days == 7:
+            self.show_details = True
+            self.save()
+        return
 
-	def expired(self):
-		return self.expiry_date.__lt__(timezone.now())
+    def expired(self):
+        return self.expiry_date.__lt__(timezone.now())
 
-	def chat_messages(self, user_position):
-		list_ = {'first': self.first_deleted,
-		         'second': self.second_deleted}
-		return ChatMessage.objects.all().filter(Q(~Q(id__in=list_[user_position]),
-		                                          chat_id=self.id)).order_by('datetime')
+    def chat_messages(self, user_position):
+        list_ = {'first': self.first_deleted,
+                 'second': self.second_deleted}
+        return ChatMessage.objects.all().filter(Q(~Q(id__in=list_[user_position]),
+                                                  chat_id=self.id)).order_by('datetime')
 
-	def get_chat_file(self, user):
-		user_position = self.position(user)
-		other_user = self.get_receiver(user)
-		chat_messages = self.chat_messages(user_position)
-		text_file = open(f'Chat_with_{other_user.username}.txt', 'w+')
-		text_file.write(f'Chat Between {user.username} and {other_user.username}.\n\n')
-		for msg in chat_messages:
-			text_file.write(f"{msg.sender.username} "
-			                f"({msg.datetime.time().strftime('%I:%M %p')}): {msg.actual_text}\n")
-		text_file.close()
+    def get_chat_file(self, user):
+        user_position = self.position(user)
+        other_user = self.get_receiver(user)
+        chat_messages = self.chat_messages(user_position)
+        text_file = open(f'Chat_with_{other_user.username}.txt', 'w+')
+        text_file.write(f'Chat Between {user.username} and {other_user.username}.\n\n')
+        for msg in chat_messages:
+            text_file.write(f"{msg.sender.username} "
+                            f"({msg.datetime.time().strftime('%I:%M %p')}): {msg.actual_text}\n")
+        text_file.close()
         return {
             "content": base64.b64encode(text_file.read().encode()).decode(),
             "type": "text/plain",
             "filename": f'Chat_with_{other_user.username}.txt'
         }
 
-	def get_chat(self, user_position):
-		self.self_delete()
-		self.show_detail()
-		chat_message_items = self.chat_messages(user_position)
+    def get_chat(self, user_position):
+        self.self_delete()
+        self.show_detail()
+        chat_message_items = self.chat_messages(user_position)
 
-		data = tuple([msg.serialized_data for msg in chat_message_items])
-		status = ''
-		if user_position == 'first':
-			status = User.objects.get(id=self.second_user_id).status
-		if user_position == 'second':
-			status = User.objects.get(id=self.first_user_id).status
-		data = {'chat_id': self.id, 'expired': self.expired(), 'status': status, 'chat_list': data}
-		return data
+        data = tuple([msg.serialized_data for msg in chat_message_items])
+        status = ''
+        if user_position == 'first':
+            status = User.objects.get(id=self.second_user_id).status
+        if user_position == 'second':
+            status = User.objects.get(id=self.first_user_id).status
+        data = {'chat_id': self.id, 'expired': self.expired(), 'status': status, 'chat_list': data}
+        return data
 
-	def get_receiver(self, user):
-		if self.first_user_id == user.id:
-			return User.objects.get(id=self.second_user_id)
-		else:
-			return User.objects.get(id=self.first_user_id)
+    def get_receiver(self, user):
+        if self.first_user_id == user.id:
+            return User.objects.get(id=self.second_user_id)
+        else:
+            return User.objects.get(id=self.first_user_id)
 
-	def position(self, user):
-		return "first" if self.first_user_id == user.id else "second"
+    def position(self, user):
+        return "first" if self.first_user_id == user.id else "second"
 
-	@property
-	def last_message(self):
-		return ChatMessage.objects.filter(chat=self).order_by('-datetime').first()
+    @property
+    def last_message(self):
+        return ChatMessage.objects.filter(chat=self).order_by('-datetime').first()
 
-	def last_message_text(self):
-		if self.last_message:
-			return self.last_message.actual_text
-		return None
+    def last_message_text(self):
+        if self.last_message:
+            return self.last_message.actual_text
+        return None
 
-	def encrypt(self, data):
-		from cryptography.fernet import Fernet
-		return Fernet(self.secret).encrypt(data.encode()).decode()
+    def encrypt(self, data):
+        from cryptography.fernet import Fernet
+        return Fernet(self.secret).encrypt(data.encode()).decode()
 
-	def decrypt(self, cipher_text):
-		from cryptography.fernet import Fernet
-		return Fernet(self.secret).decrypt(cipher_text.encode()).decode()
+    def decrypt(self, cipher_text):
+        from cryptography.fernet import Fernet
+        return Fernet(self.secret).decrypt(cipher_text.encode()).decode()
 
-	def no_unread_msg(self, user):
-		receiver = self.get_receiver(user)
-		position = self.position(user)
-		list_ = {'first': self.first_deleted, 'second': self.second_deleted}
-		return ChatMessage.objects.filter(chat=self, sender=receiver, send_status='sent') \
-			.filter(~Q(id__in=list_[position])).count()
+    def no_unread_msg(self, user):
+        receiver = self.get_receiver(user)
+        position = self.position(user)
+        list_ = {'first': self.first_deleted, 'second': self.second_deleted}
+        return ChatMessage.objects.filter(chat=self, sender=receiver, send_status='sent') \
+            .filter(~Q(id__in=list_[position])).count()
 
-	def first_msg_today(self):
-		return ChatMessage.objects.filter(chat=self). \
-			filter(datetime=timezone.now().today()).order_by('id').first()
+    def first_msg_today(self):
+        return ChatMessage.objects.filter(chat=self). \
+            filter(datetime=timezone.now().today()).order_by('id').first()
 
 
 class ChatMessage(models.Model):
-	sender = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING)
-	text = models.TextField()
-	datetime = models.DateTimeField()
-	send_status = models.CharField(max_length=20, choices=(('sent', 'sent'), ('delivered', 'delivered')))
-	chat = models.ForeignKey(ChatThread, on_delete=models.CASCADE)
+    sender = models.ForeignKey("Account.User", on_delete=models.DO_NOTHING)
+    text = models.TextField()
+    datetime = models.DateTimeField()
+    send_status = models.CharField(max_length=20, choices=(('sent', 'sent'), ('delivered', 'delivered')))
+    chat = models.ForeignKey(ChatThread, on_delete=models.CASCADE)
 
-	def expired(self):
-		return (timezone.now() - self.datetime).total_seconds() > 180
+    def expired(self):
+        return (timezone.now() - self.datetime).total_seconds() > 180
 
-	@property
-	def actual_text(self):
-		return self.chat.decrypt(self.text)
+    @property
+    def actual_text(self):
+        return self.chat.decrypt(self.text)
 
-	@property
-	def serialized_data(self):
-		from Chat.algorithms import profile_picture
-		return {'id': self.id,
-		        'time': self.datetime.strftime('%I:%M %p'),
-		        'date': self.datetime.strftime('%e - %b - %Y'),
-		        'message': self.actual_text,
-		        'sender_id': self.sender.id,
-		        'sender': self.sender.username,
-		        'sender_pic': profile_picture(self.sender.profile_picture),
-		        'status': self.send_status}
+    @property
+    def serialized_data(self):
+        from Chat.algorithms import profile_picture
+        return {'id': self.id,
+                'time': self.datetime.strftime('%I:%M %p'),
+                'date': self.datetime.strftime('%e - %b - %Y'),
+                'message': self.actual_text,
+                'sender_id': self.sender.id,
+                'sender': self.sender.username,
+                'sender_pic': profile_picture(self.sender.profile_picture),
+                'status': self.send_status}
